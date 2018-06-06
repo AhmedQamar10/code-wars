28b6676566ecdf4e7508b29778d44849d0ab2fa4
Kevin Grayson <k.grayson.ms@gmail.com>
2018-06-05 21:07:44 -0700

    Lab complete

--- 5-regex-validate-PIN-code/solution.js
+++ 5-regex-validate-PIN-code/solution.js
@@ -1,4 +1,9 @@
 /* eslint-disable-next-line */
 function validatePIN (pin) {
-  
+    var reg = /^\d{4}(?:\d{2})?$/;
+    if(reg.test(pin)) {
+        return true;
+    } else {
+        return false;
+    }
 }
