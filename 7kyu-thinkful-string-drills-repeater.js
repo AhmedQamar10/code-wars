93119d4d004960a6f9cb8e028bed804e463f4828
Kevin Grayson <k.grayson.ms@gmail.com>
2018-10-30 23:05:26 -0700

    Thinkful string drills repeater solution

---
+++ 7kyu-thinkful-string-drills-repeater.js
@@ -0,0 +1,4 @@
+// Write a class function named repeat() that takes two arguments (a string and a long integer),
+//and returns a new string where the input string is repeated that many times.
+
+const repeater = (string, n) => string.repeat(n);
