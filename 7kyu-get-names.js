a51d727017c408be7aef08ea6096646669ee8a53
Kevin Grayson <k.grayson.ms@gmail.com>
2018-11-14 16:28:29 -0800

    get names solution

---
+++ 7kyu-get-names.js
@@ -0,0 +1,4 @@
+// The following code is not giving the expected results. Can you figure out what the issue is?
+// The following is an example of data that would be passed in to the function.
+
+const getNames = data => data.map(person => person.name);
