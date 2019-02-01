db4670d0e12579ef2783125d0512d27b0cf4eb06
Kevin Grayson <k.grayson.ms@gmail.com>
2019-02-01 10:26:37 -0800

    Find the next perfect square solution

---
+++ 7kyu-find-the-next-perfect-square.js
@@ -0,0 +1,5 @@
+// You might know some pretty large perfect squares. But what about the NEXT one?
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+// If the parameter is itself not a perfect square, than -1 should be returned. You may assume the parameter is positive.
+
+const findNextSquare = (sq, res = Math.pow(Math.sqrt(sq) + 1, 2)) => Number.isInteger(res) ? res : -1;
