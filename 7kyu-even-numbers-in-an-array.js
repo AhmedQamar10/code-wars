1e7f8ad1edd0c69fc69ff6b0871e6698ce1d9a9e
Kevin Grayson <k.grayson.ms@gmail.com>
2019-02-02 21:00:12 -0800

    even numbers in an array solution

---
+++ 7kyu-even-numbers-in-an-array.js
@@ -0,0 +1,7 @@
+// Given an array of digitals numbers, return a new array of length number containing the last even numbers from the original array (in the same order).
+// The original array will be not empty and will contain at least "number" even numbers.
+
+function evenNumbers(array, number) {
+  let evens = array.filter(n => n % 2 === 0);
+  return evens.slice(evens.length - number);
+}
