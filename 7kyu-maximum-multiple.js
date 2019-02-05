34411fffff339b64fed0e014bb984deb95b34943
Kevin Grayson <k.grayson.ms@gmail.com>
2019-02-04 23:35:15 -0800

    maximum multiple solution

---
+++ 7kyu-maximum-multiple.js
@@ -0,0 +1,12 @@
+// Given a Divisor and a Bound , Find the largest integer N , Such That ,
+// Conditions :
+// N is divisible by divisor
+// N is less than or equal to bound
+// N is greater than 0.
+
+function maxMultiple(divisor, bound){
+  if(divisor > bound) return 0;
+  for(let i = bound; i > 0; i--) {
+    if(i % divisor === 0) return i;
+  }
+}
